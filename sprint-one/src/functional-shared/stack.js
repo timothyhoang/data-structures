56281988d123438849b195a14f4dad836cc65d42
Yuheng Huang <yuheng08@gmail.com>
2017-08-23 13:21:57 -0700

    Implemented size function in a function-shared pattern stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,12 +1,9 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = {};
-  var storage = {};
-  var count = 0;
+  var someInstance = {count: 0, storage: {}};
 
   extend(someInstance, stackMethods);
-
   return someInstance;
 };
 
