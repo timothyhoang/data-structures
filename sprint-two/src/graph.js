0bf5e0c99d2470ef3849aab77e740362f8799a42
Hack Reactor Student <student@hackreactor.com>
2017-08-23 20:59:34 -0700

    Implemented graphs

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,34 +2,67 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.vertices = [];
+  this.edgeMatrix = [];
+  
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.vertices.push(node);
+  this.edgeMatrix.push([]);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.vertices.includes(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var index = _.indexOf(this.vertices, node);
+  
+  for (var edges of this.edgeMatrix) {
+    edges.splice(index, 1);
+  }
+  
+  this.edgeMatrix.splice(index, 1);
+  this.vertices.splice(index, 1);
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var fromIndex = _.indexOf(this.vertices, fromNode);
+  var toIndex = _.indexOf(this.vertices, toNode);
+  
+  if (fromIndex === -1 || toIndex === -1) {
+    return false;
+  } 
+  
+  return Boolean(this.edgeMatrix[fromIndex][toIndex] || this.edgeMatrix[toIndex][fromIndex]);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  var fromIndex = _.indexOf(this.vertices, fromNode);
+  var toIndex = _.indexOf(this.vertices, toNode);
+  
+  this.edgeMatrix[fromIndex][toIndex] = true;
+  this.edgeMatrix[toIndex][fromIndex] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var fromIndex = _.indexOf(this.vertices, fromNode);
+  var toIndex = _.indexOf(this.vertices, toNode);
+  
+  this.edgeMatrix[fromIndex][toIndex] = undefined;
+  this.edgeMatrix[toIndex][fromIndex] = undefined;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.forEach(this.vertices, cb);
 };
 
 /*
