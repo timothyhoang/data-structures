0f6473df035c7d54cf99da6fadc7511d97eac7d1
Hack Reactor Student <student@hackreactor.com>
2017-08-23 17:41:54 -0700

    Implemented functional-shared/prototypal/psuedoclassical stacks and queues

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    numElements: 0,
+    storage: {},
+    head: 0
+  };
+  
+  _.extend(someInstance, queueMethods);
+  
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.head + this.numElements++] = value;
+  },
+  
+  dequeue: function() {
+    if (this.numElements) {
+      this.numElements--;
+      return this.storage[this.head++];
+    }
+  },
+  
+  size: function() {
+    return this.numElements;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    numElements: 0,
+    storage: {}
+  };
+  
+  _.extend(someInstance, stackMethods);
+  
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.numElements++] = value;
+  },
+  
+  pop: function() {
+    if (this.numElements) {
+      this.numElements--;
+      return this.storage[this.numElements];
+    }
+  },
+  
+  size: function() {
+    return this.numElements;
+  }
+};
 
 
