3cf3128e935cdae6b061a636daabec463ec997c5
Hack Reactor Student <student@hackreactor.com>
2017-08-23 21:16:10 -0700

    Implemented binary search tree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,57 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(bstMethods);
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
+  return bst;
+};
+
+bstMethods = {
+  insert: function(value) {
+    if (this.value >= value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  },
+  
+  contains: function(value) {
+    if (this.value > value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value);
+      }
+    } else if (this.value < value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    } else {
+      return true;
+    }
+  }, 
+  
+  depthFirstLog: function(callback) {
+    callback(this.value);
+    
+    if (this.left) {
+      this.left.depthFirstLog(callback);
+    }
+    
+    if (this.right) {
+      this.right.depthFirstLog(callback);
+    }
+  }
 };
 
 
