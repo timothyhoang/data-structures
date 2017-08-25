a948f3c9a78f5cc841ecfe2c8ff0711abb3fbcff
Hack Reactor Student <student@hackreactor.com>
2017-08-25 11:35:34 -0700

    Changed implementation of hashtables to use buckets of tuples and changed load factor

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,56 +8,81 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
+    
   if (this._storage.get(index)) {
-    this._storage.get(index).set(k, v);
-    this._numElements++;
-    if (this._numElements === this._limit - 1) {
-      this.resize(2 * this._limit);
-    }
+    if (this.retrieve(k)) {
+      this.remove(k);
+    }  
+    this._storage.get(index).push([JSON.stringify(k), v]);
   } else {
-    var map = new Map([[k, v]]);
-    this._storage.set(index, map);
-    this._numElements++;
+    var bucket = [[JSON.stringify(k), v]];
+    this._storage.set(index, bucket);
+  }
+  
+  this._numElements++;
+  
+  if (this._numElements === Math.ceil(.75 * this._limit)) {
+    this.resize(2 * this._limit);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage.get(index)) {
-    return this._storage.get(index).get(k);
+  var bucket = this._storage.get(index);
+  var bucketIndex = this.findBucketIndex(k);
+  
+  if (bucket && bucketIndex !== -1) {
+    return bucket[bucketIndex][1];
   }
+};
+
+HashTable.prototype.findBucketIndex = function (k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  
+  if (bucket) {
+    var indexOfKey = bucket.findIndex(function(entry) {
+      return entry[0] === JSON.stringify(k);
+    });
+  }
+    
+  return indexOfKey;  
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var bucketIndex = this.findBucketIndex(k);
   
-  if (this._storage.get(index) && this._storage.get(index).has(k)) {
-    this._storage.get(index).delete(k);
+  if (bucket && bucketIndex !== -1) {
+    this._storage.get(index).splice(bucketIndex, 1);
     this._numElements--;
     
-    var loadFactor = Math.ceil(this._limit / 8);
-    if (this._numElements <= loadFactor) {
+    var loadFactor = Math.ceil(this._limit / 4);
+    
+    if (this._numElements === loadFactor) {
       this.resize(Math.ceil(this._limit / 2));
     }
   }
 };
 
 HashTable.prototype.resize = function(newLimit) {
-  var newHashTable = new HashTable();
-  newHashTable._limit = newLimit;
-  newHashTable._storage = LimitedArray(newLimit);
-  
-  this._storage.each(function(map) {
-    if (map) {
-      for (let entry of map.entries()) {
-        newHashTable.insert(entry[0], entry[1]);
+  if (newLimit >= 8) {
+    var newHashTable = new HashTable();
+    newHashTable._limit = newLimit;
+    newHashTable._storage = LimitedArray(newLimit);
+    
+    this._storage.each(function(bucket) {
+      if (bucket) {
+        for (let entry of bucket) {
+          newHashTable.insert(JSON.parse(entry[0]), entry[1]);
+        }
       }
-    }
-  });
-  
-  this._limit = newHashTable._limit;
-  this._storage = newHashTable._storage;
+    });
+    
+    this._limit = newHashTable._limit;
+    this._storage = newHashTable._storage;
+  }
 };
 
 
