6a1842b21ecb1f7a52538cb0405ca52b7ee9acd0
Hack Reactor Student <student@hackreactor.com>
2017-08-25 19:12:02 -0700

    Implemented skeleton for doubly linked list and spec

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,29 @@
+var DoublyLinkedList = function() {
+  this.sentinel = new Node();
+};
+
+DoublyLinkedList.prototype.addToHead = function(value) {
+  
+};
+
+DoublyLinkedList.prototype.addToTail = function(value) {
+  
+};
+
+DoublyLinkedList.prototype.removeHead = function() {
+  
+};
+
+DoublyLinkedList.prototype.removeTail = function() {
+  
+};
+
+DoublyLinkedList.prototype.contains = function(value) {
+  
+};
+
+var Node = function(value) {
+  this.value = value;
+  this.prev = null;
+  this.next = null;
+};
